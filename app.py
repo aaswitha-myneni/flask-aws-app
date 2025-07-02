c4dd0cea120ea33191ee005de7b94e7e127c6484
Ubuntu <ubuntu@ip-172-31-91-152.ec2.internal>
2025-07-02 17:12:09 +0000

    Updated welcome message to trigger deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Aaswitha! Welcome to your AWS App."
+    return "Hello Aaswitha! This is my updated AWS App."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
